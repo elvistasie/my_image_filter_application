84daad84c135d8f9c7c141883600e69bad167ccf
Elvis Tasie <elvistasie@gmail.com>
2022-06-25 16:17:29 +0100

    errors corrected

--- src/server.ts
+++ src/server.ts
@@ -26,9 +26,9 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   //    image_url: URL of a publicly accessible image
   // RETURNS
   //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
-  
-  app.get("/filteredimage", async (req, res) => {
-    const {image_url} = req.query;
+
+  app.get("/filteredimage", async ( req: express.Request, res: express.Response ) => {
+    const { image_url } = req.query;
     
     if(!image_url) {
       return res.status(400).json({"Provide a valid image url": false});
