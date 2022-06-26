d1f8aaf5498ca1dcf923382e0330b9d18afb5895
Elvis Tasie <elvistasie@gmail.com>
2022-06-26 07:42:37 +0100

    Update util.ts

--- src/util/util.ts
+++ src/util/util.ts
@@ -11,15 +11,12 @@ import  axios, { AxiosResponse}  from "axios";
 //    an absolute path to a filtered image locally saved file
 export async function filterImageFromURL(inputURL: string): Promise<string> {
   return new Promise(async (resolve, reject) => {
-    const response: AxiosResponse = await axios({
-      method: 'get',
-      url: inputURL,
-      responseType: 'arraybuffer'
-    });
+
+ 
     try {
-      const photo = await Jimp.read(response.data);
+      const photo = await Jimp.read(inputURL);
       const outpath =
-        "/tmp/filtered." + Math.floor(Math.random() * 2000) + ".jpg";
+        "/tmp/filtered." + Math.floor(Math.random() * 2000) + ".jpg"; 
       await photo
         .resize(256, 256) // resize
         .quality(60) // set JPEG quality
